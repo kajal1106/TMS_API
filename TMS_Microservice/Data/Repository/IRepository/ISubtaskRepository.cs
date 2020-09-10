11f6eeff3fa0a26ab0edcd4cec16d9685a89b23c
Kajal Singh <singh.kajal940@gmail.com>
2020-09-10 12:53:42 +0100

    Created interface to perform crud operation functions

---
+++ TMS_Microservice/Data/Repository/IRepository/ISubtaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMS_Microservice.Model;
+
+namespace TMS_Microservice.Data.Repository.IRepository
+{
+    interface ISubtaskRepository
+    {
+        ICollection<Subtask> GetSubtasks();
+        Subtask GetSubtask(int subtaskId);
+        bool CreateSubtask(Subtask subtask);
+        bool UpdateSubtask(Subtask subtask);
+        bool DeleteSubtask(Subtask subtask);
+        bool Save();
+    }
+}
---
+++ TMS_Microservice/Data/Repository/IRepository/ITaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMS_Microservice.Model;
+
+namespace TMS_Microservice.Data.Repository.IRepository
+{
+    interface ITaskRepository
+    {
+        ICollection<Task> GetTasks();
+        Task GetTask(int taskId);
+        bool CreateTask(Task task);
+        bool UpdateTask(Task task);
+        bool DeleteTask(Task task);
+        bool Save();
+    }
+}
