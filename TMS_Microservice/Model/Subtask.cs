256fe1213da85ac49c00137d085bd029d9ba1545
Kajal Singh <singh.kajal940@gmail.com>
2020-09-10 12:38:50 +0100

    Task and SubTask model created

---
+++ TMS_Microservice/Model/Subtask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+
+namespace TMS_Microservice.Model
+{
+    public class Subtask
+    {
+        [Key]
+        public int Id { get; set; }
+        public int TaskId { get; set; }
+
+        [ForeignKey("TaskId")]
+        [IgnoreDataMember]
+        public Task Task { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string State { get; set; }
+        public DateTime Start_date { get; set; }
+        public DateTime Finish_date { get; set; }
+
+    }
+}
---
+++ TMS_Microservice/Model/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMS_Microservice.Model
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string State { get; set; }
+        public DateTime Start_date { get; set; }
+        public DateTime Finish_date { get; set; }
+    }
+}
