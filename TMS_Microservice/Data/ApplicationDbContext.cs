ee08c153d21756dfb31a2ceb5e4c9ee2cdf8d28b
Kajal Singh <singh.kajal940@gmail.com>
2020-09-10 12:47:38 +0100

    Created Dbcontext Entity framework which can be used to query and save instances of your entities to a database.

---
+++ TMS_Microservice/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TMS_Microservice.Model;
+
+namespace TMS_Microservice.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public DbSet<Subtask> Subtasks { get; set; }
+    }
+}
